310fd098b6dc47fd74b796f21fd8bc52c75fa406
G0aty695 <121062577+TheG0at685@users.noreply.github.com>
2022-12-20 20:54:20 -0500

    Seriously, please stop.

---
+++ Levels/Bullet.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+
+var velocity = Vector2()
+var speed = 500
+
+# determines if it is the player or the enemy bullet
+var side
+
+var current_level
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector2()
+	velocity = Vector2(-speed, 0).rotated(rotation)
+	velocity = move_and_slide(velocity)
+	
+	if $Collision.overlaps_body(get_parent().get_node("Player").current_level.get_node("TileMap")):
+		# If touching the map delete the instance and its object in the level array
+		if side == "player":
+			get_parent().player_bullets.erase(self)
+		else:
+			get_parent().enemy_bullets.erase(self)
+		queue_free()
+		
+	if side == "enemy":
+		# Logic for specifcily enenmy bullets
+		if $Collision.overlaps_body(get_parent().get_node("Player")):
+			get_parent().enemy_bullets.erase(self)
+			queue_free()
+	
+	if side == "player":
+		# Logic for player bullets
+		for enemy in get_parent().enemys:
+			if $Collision.overlaps_body(enemy):
+				get_parent().player_bullets.erase(self)
+				queue_free()
+
---
+++ Levels/Level.gd
@@ -0,0 +1,75 @@
+extends Node2D
+
+
+var refills = []
+var player_bullets = []
+var enemy_bullets = []
+var enemys = []
+
+
+var level1
+var level_instance
+var level = 1
+var pause_menu = load("res://Pause menu.tscn")
+var menu_instance = pause_menu.instance()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	level1()
+	add_child(menu_instance)
+	# Draw the pause menu after everything else to put it at the front of the scene
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func change_level():
+	remove_child(level_instance)
+	menu_instance.queue_free()
+	for e in enemys:
+		remove_child(e)
+	if level == 1:
+		level1()
+	if level == 2:
+		level2()
+	if level == 3:
+		level3()
+	if level == 4:
+		level4()
+	if level == 5:
+		level5()
+	pause_menu = load("res://Pause menu.tscn")
+	menu_instance = pause_menu.instance()
+	add_child(menu_instance)
+		
+func level1():
+	level1 = load("res://Level1.tscn")
+	level_instance = level1.instance()
+	add_child(level_instance)
+	
+func level2():
+	level1 = load("res://Level2.tscn")
+	level_instance = level1.instance()
+	add_child(level_instance)
+	
+func level3():
+	level1 = load("res://Level3.tscn")
+	level_instance = level1.instance()
+	add_child(level_instance)
+	
+func level4():
+	level1 = load("res://Level4.tscn")
+	level_instance = level1.instance()
+	add_child(level_instance)
+	for tur in level_instance.get_node("Turrents").get_used_cells():
+		var turrent = load("res://Turrent.tscn")
+		var turrent_instance = turrent.instance()
+		add_child(turrent_instance)
+		# Don't ask me why it muiltiplys by 31. It works, so we don't touch it
+		turrent_instance.position = Vector2(tur.x * 31, tur.y * 31)
+		enemys.append(turrent_instance)
+		
+func level5():
+	level1 = load("res://Level5.tscn")
+	level_instance = level1.instance()
+	add_child(level_instance)
